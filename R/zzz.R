d5672aec68577c2c5ca4404584c8f95da5f288b6
lshep <lori.shepherd@roswellpark.org>
2020-06-01 10:07:39 -0400

    User requested deprecation
      - replaced with metaseqR2

--- R/zzz.R
+++ R/zzz.R
@@ -8,3 +8,11 @@ assign("LOGGER",NULL,envir=meta.env)
 #        "There is an incompatibility with the latest\nversion of baySeq which ",
 #        "we are fixing right now...\n\n")
 #}
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname,
+        "3.13. Package functionality moved to Bioconductor package metaseqR2.")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
